2dd0933b16abdec378130c54504922f75ce16428
Erikson <erikson.bahr@gmail.com>
2021-06-18 19:19:16 -0300

    The app now loops in the conversion question.

--- main.go
+++ main.go
@@ -28,9 +28,18 @@ func main() {
 		if err != nil {
 			printError(errReadingInput)
 		}
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanln(&shouldConvertAgain)
+		if err != nil {
+			printError(errReadingInput)
+		}
 
-		if shouldConvertAgain != "Y" {
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
